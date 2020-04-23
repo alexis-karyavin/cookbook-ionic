bcf403e98cdcd6261aab9fa1ebbd3f563acf0b63
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-04-24 01:24:26 +0300

    Add new key

--- src/app/core/model/dish.model.ts
+++ src/app/core/model/dish.model.ts
@@ -1,6 +1,8 @@
 export class DishModel {
   public name: string;
   public url: string;
+  public images: Array<string> = [];
+  public cookingTime: string;
   public ingredients: Array<{ingredientName: string, count: string}>;
   public cookingMethod: Array<string>;
 }
@@ -10,6 +12,8 @@ export class AdapterDish {
     const dish = new DishModel();
     dish.name = res.dishName;
     dish.url = res.url;
+    dish.cookingTime = res.cookingTime;
+    dish.images = res.images;
     dish.ingredients = res.ingredients;
     dish.cookingMethod = res.cookingMethod;
     return dish;
