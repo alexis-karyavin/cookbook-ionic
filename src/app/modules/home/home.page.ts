cd8d45bfc5fc729f451fc91580a5fe742af35115
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-04-24 00:58:18 +0300

    Show categories menu

--- src/app/modules/home/home.page.ts
+++ src/app/modules/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CookbookService} from '../../core/services/cookbook.service';
+import {CategoryModel} from '../../core/model/category.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public categories: CategoryModel[];
+  constructor(private cookbookService: CookbookService) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.categories = this.cookbookService.getCategories();
+  }
 
 }
