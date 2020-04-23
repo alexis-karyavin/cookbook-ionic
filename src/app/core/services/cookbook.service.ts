7830e0c24063ae8d7ae91afd4dd1ea1e72272de7
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-04-24 00:57:50 +0300

    Add fun getDishes

--- src/app/core/services/cookbook.service.ts
+++ src/app/core/services/cookbook.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 // @ts-ignore
 import data from '../../../assets/data/data.json';
-import {AdapterCategory, CategoryModel} from "../model/category.model";
+import {AdapterCategory, CategoryModel} from '../model/category.model';
+import {DishModel} from '../model/dish.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,10 +15,19 @@ export class CookbookService {
     data.forEach(item => {
       this.categories.push(AdapterCategory.adapt(item));
     });
+    this.getDishes()
   }
-
+  public getCategories(): CategoryModel[] {
+    return this.categories;
+  }
   public getCategory(name: string): CategoryModel {
-
     return new CategoryModel();
+  }
+  public getDishes(): DishModel[] {
+    const dishes = [];
+    this.categories.forEach(category => {
+     category.dishes.forEach(dish => dishes.push(dish));
+    });
+    return dishes;
   }
 }
